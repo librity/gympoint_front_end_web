b787b25c1f7f93d19bd7fbede0b9383cda6a3e40
Luis Geniole <luis.geniole@gmail.com>
2019-11-17 02:11:49 -0300

    finish ManagePlans

--- src/pages/Plans/ManagePlans/index.js
+++ src/pages/Plans/ManagePlans/index.js
@@ -1,11 +1,122 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { MdAdd } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
-import { Container } from './styles';
+import history from '~/services/history';
+import api from '~/services/api';
+
+import { formatPricePtBr } from '~/util/format';
+
+import { Container, Content, ProductTable } from './styles';
 
 export default function ManagePlans() {
+  const [plans, setPlans] = useState([]);
+
+  useEffect(() => {
+    const loadPlans = async () => {
+      const response = await api.get('plans');
+
+      const data = response.data.map(plan => ({
+        ...plan,
+        formattedMonthlyPrice: formatPricePtBr(plan.price),
+      }));
+
+      setPlans(data);
+    };
+
+    loadPlans();
+  }, []);
+
+  const navigateNewStudent = () => {
+    history.push('/plans/new');
+  };
+
+  const handleSearch = async ({ queryName }) => {
+    // console.tron.log(queryName);
+
+    const response = await api.get('/plans', {
+      params: { name: queryName },
+    });
+
+    setPlans(response.data);
+  };
+
+  const handleDeleteStudent = async ({ id }) => {
+    if (window.confirm('Você tem certeza que deseja remover este plano?')) {
+      try {
+        await api.delete(`/plans/${id}`);
+
+        handleSearch('');
+
+        toast.success('Aluno apagado com sucesso!');
+      } catch (err) {
+        toast.error('Falha na ataulização, verifique os dados!');
+      }
+    }
+  };
+
   return (
     <Container>
-      <h1>ManagePlans</h1>
+      <div>
+        <h1>Gerenciando planos</h1>
+
+        <aside>
+          <button
+            className="navigateNewStudent"
+            type="button"
+            onClick={navigateNewStudent}
+          >
+            <MdAdd size={20} color="#fff" />
+            CADASTRAR
+          </button>
+        </aside>
+      </div>
+      <Content>
+        <ProductTable>
+          <thead>
+            <tr>
+              <th className="id">ID</th>
+              <th className="title">TÍTULO</th>
+              <th className="duration">DURAÇÃO</th>
+              <th className="price">VALOR p/ MÊS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {plans.map(plan => (
+              <tr>
+                <td className="id">
+                  <strong>{plan.id}</strong>
+                </td>
+                <td className="title">
+                  <strong>{plan.title}</strong>
+                </td>
+                <td className="duration">
+                  <strong>{plan.duration}</strong>
+                </td>
+                <td className="price">
+                  <strong>{plan.formattedMonthlyPrice}</strong>
+                </td>
+                <td className="options">
+                  <Link
+                    to={{ pathname: '/plans/edit', state: { plan } }}
+                    className="edit"
+                  >
+                    editar
+                  </Link>
+                  <button
+                    className="delete"
+                    type="button"
+                    onClick={() => handleDeleteStudent(plan)}
+                  >
+                    apagar
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </ProductTable>
+      </Content>
     </Container>
   );
 }
---
+++ src/util/format.js
@@ -0,0 +1,4 @@
+export const { format: formatPricePtBr } = new Intl.NumberFormat('pt-BR', {
+  style: 'currency',
+  currency: 'BRL',
+});
