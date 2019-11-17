1629ae7cc6c744113eb0f9844dcd3c654c5bbe8a
Luis Geniole <luis.geniole@gmail.com>
2019-11-17 03:26:49 -0300

    finish NewPlan

--- src/pages/Plans/NewPlan/index.js
+++ src/pages/Plans/NewPlan/index.js
@@ -1,11 +1,133 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { MdChevronLeft, MdDone } from 'react-icons/md';
+import { Form, Input } from '@rocketseat/unform';
+import { toast } from 'react-toastify';
+import * as Yup from 'yup';
 
-import { Container } from './styles';
+import history from '~/services/history';
+import api from '~/services/api';
+
+import { formatPricePtBr } from '~/util/format';
+
+import { Container, Content } from './styles';
+
+const submitNewPlanSchema = Yup.object().shape({
+  title: Yup.string().required('O título é obrigatório!'),
+
+  duration: Yup.number()
+    .integer('Insira número inteiro!')
+    .min(1, 'Duração mínima: 1 mês!')
+    .max(24, 'Duração máxima: 24 mês!')
+    .required('A duração é obrigatória!'),
+
+  price: Yup.number('Insira número!')
+    .min(0.0, 'Preço mínimo: R$ 0,00!')
+    .max(1000.0, 'Preço máximo: R$ 1000,00!')
+    .required('O preço é obrigatório!'),
+});
 
 export default function NewPlan() {
+  const [planDuration, setPlanDuration] = useState(0);
+  const [planPrice, setPlanPrice] = useState(0);
+
+  const totalPrice = useMemo(() => planDuration * planPrice, [
+    planDuration,
+    planPrice,
+  ]);
+
+  const navigateManagePlans = () => {
+    history.push('/plans');
+  };
+
+  const submitNewPlan = async ({ title, duration, price }) => {
+    try {
+      console.tron.log(title, duration, price);
+
+      const response = await api.post('/plans', {
+        title,
+        duration,
+        price,
+      });
+      console.tron.log(response);
+
+      toast.success('Aluno cadastrado com sucesso!');
+      history.push('/plans');
+    } catch (err) {
+      // console.tron.log(err);
+      toast.error('Falha no cadastro, verifique os dados!');
+    }
+  };
+
   return (
     <Container>
-      <h1>NewPlan</h1>
+      <div>
+        <h1>Cadastro de plano</h1>
+
+        <aside>
+          <button
+            className="navigateManagePlans"
+            type="button"
+            onClick={navigateManagePlans}
+          >
+            <MdChevronLeft size={20} color="#fff" />
+            VOLTAR
+          </button>
+          <button
+            className="submitNewPlan"
+            type="submit"
+            form="submitNewPlanForm"
+          >
+            <MdDone size={20} color="#fff" />
+            SALVAR
+          </button>
+        </aside>
+      </div>
+      <Content>
+        <Form
+          schema={submitNewPlanSchema}
+          onSubmit={submitNewPlan}
+          id="submitNewPlanForm"
+        >
+          <label htmlFor="title">TÍTULO DO PLANO</label>
+          <Input id="title" name="title" type="text" required />
+          <span>
+            <span>
+              <label htmlFor="duration">DURAÇÃO (em meses)</label>
+              <Input
+                id="duration"
+                name="duration"
+                type="number"
+                required
+                value={planDuration}
+                onInput={e => setPlanDuration(e.target.value)}
+              />
+            </span>
+            <span>
+              <label htmlFor="price">PREÇO MENSAL</label>
+              <Input
+                id="price"
+                name="price"
+                type="number"
+                step="0.01"
+                required
+                value={planPrice}
+                onInput={e => setPlanPrice(e.target.value)}
+              />
+            </span>
+            <span>
+              <label htmlFor="total_price">PREÇO TOTAL</label>
+              <Input
+                id="total_price"
+                name="total_price"
+                type="number"
+                step="0.01"
+                readOnly
+                value={totalPrice}
+              />
+            </span>
+          </span>
+        </Form>
+      </Content>
     </Container>
   );
 }
