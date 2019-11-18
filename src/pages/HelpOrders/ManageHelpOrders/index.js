1b51351f25ba908611e171f2fc168cfe3c121ef8
Luis Geniole <luis.geniole@gmail.com>
2019-11-17 23:06:29 -0300

    Finish ManageHelpOrder Structure

--- src/pages/HelpOrders/ManageHelpOrders/index.js
+++ src/pages/HelpOrders/ManageHelpOrders/index.js
@@ -1,11 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { MdAdd, MdSearch } from 'react-icons/md';
+import { Form, Input } from '@rocketseat/unform';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import * as Yup from 'yup';
 
-import { Container } from './styles';
+import api from '~/services/api';
+
+import { Container, Scroll, ProductTable } from './styles';
+
+const helpOrderAnswerSchema = Yup.object().shape({
+  queryName: Yup.string(),
+});
 
 export default function ManageHelpOrders() {
+  const [helpOrders, setHelpOrders] = useState([]);
+
+  useEffect(() => {
+    const loadHelpOrders = async () => {
+      const response = await api.get('help_orders');
+
+      setHelpOrders(response.data);
+    };
+
+    loadHelpOrders();
+  }, []);
+
+  const handleSearch = async ({ queryName }) => {
+    const response = await api.get('/help_orders', {
+      params: { name: queryName },
+    });
+
+    setHelpOrders(response.data);
+  };
+
+  const handleAnswerHelpOrder = async () => {};
+
   return (
     <Container>
-      <h1>ManageHelpOrders</h1>
+      <div className="pageHeader">
+        <h1>Pedidos de auxílio</h1>
+      </div>
+      <Scroll>
+        <ProductTable>
+          <thead>
+            <tr>
+              <th className="studen_name">ALUNO</th>
+            </tr>
+          </thead>
+          <tbody>
+            {helpOrders.map(helpOrder => (
+              <tr>
+                <td className="studen_name">
+                  <strong>{helpOrder.student.name}</strong>
+                </td>
+
+                <td className="options">
+                  <button
+                    className="edit"
+                    type="button"
+                    onClick={() => handleAnswerHelpOrder(helpOrder)}
+                  >
+                    responder
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </ProductTable>
+      </Scroll>
     </Container>
   );
 }
--- src/pages/HelpOrders/ManageHelpOrders/styles.js
+++ src/pages/HelpOrders/ManageHelpOrders/styles.js
@@ -1,3 +1,101 @@
 import styled from 'styled-components';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import { darken } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .pageHeader {
+    width: 40%;
+    height: 100%;
+    margin: 40px 0 20px 0;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: baseline;
+
+    h1 {
+      font-size: 24px;
+      font-weight: bold;
+      color: #444444;
+      text-align: right;
+    }
+  }
+`;
+
+export const Scroll = styled(PerfectScrollbar)`
+  padding: 30px;
+  background: #fff;
+  border-radius: 4px;
+  width: 40%;
+  margin-top: 20px;
+  max-height: 75vh;
+`;
+
+export const ProductTable = styled.table`
+  width: 100%;
+  padding: 5px 15px;
+
+  thead th {
+    text-align: left;
+    font-size: 16px;
+    font-weight: bold;
+    color: #444444;
+  }
+
+  tbody {
+    tr td {
+      text-align: left;
+      padding: 15px 0;
+      border-bottom: 1px solid #eee;
+
+      strong {
+        font-size: 16px;
+        font-weight: normal;
+        color: #666666;
+        text-align: left;
+        line-height: 20px;
+      }
+    }
+
+    tr:last-child td {
+      padding-top: 15px;
+      padding-bottom: 0;
+
+      border: none;
+    }
+  }
+
+  .studen_name {
+    width: 80%;
+    text-align: left;
+  }
+
+  .options {
+    display: flex;
+    flex-direction: row;
+    justify-content: right;
+    align-items: baseline;
+
+    button {
+      border: 0;
+      background: none;
+      font-size: 15px;
+      text-align: right;
+      margin-left: 20px;
+      padding-bottom: 5px;
+      padding-top: 5px;
+    }
+
+    .edit {
+      color: #4d85ee;
+      &:hover {
+        color: ${darken(0.1, '#4d85ee')};
+      }
+    }
+  }
+`;
